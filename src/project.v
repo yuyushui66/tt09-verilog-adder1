dfc8564c2df678154cb0f07ef027bf9a22221efa
yuyushui66 <101923681+yuyushui66@users.noreply.github.com>
2024-11-03 20:29:19 +0800

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_sec_4bit_adder(
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -15,13 +15,19 @@ module tt_um_example (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
-
+	wire [3:0] a,b,c;
+	assign a = ui_in[3:0];
+	assign b = ui_in[7:4];
+	assign c = a + b;
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
+	
+  assign uo_out[7:4] = 4'b0;
+  assign uo_out[3:0] = a + b;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, clk, rst_n,uio_in, 1'b0};
 
 endmodule
