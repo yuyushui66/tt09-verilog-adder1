9c9ce972aee0db023be5d1e1fcd09bdd1512401e
yuyushui66 <101923681+yuyushui66@users.noreply.github.com>
2024-11-03 20:30:11 +0800

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  tt_um_sec_4bit_adder user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
